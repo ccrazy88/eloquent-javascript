81d017ea573d975c7b81e5de38f4dcaf35297a52
Chrisna Aing <chrisna@chrisna.org>
2015-04-08 00:13:25 -0700

    Delete tests.

--- chapter-04/04-deep-comparison.js
+++ chapter-04/04-deep-comparison.js
@@ -32,10 +32,3 @@ function deepEqual(a, b) {
   }
   return true;
 }
-var obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, obj));
-// → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
-// → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-// → true
